e28df10e235b137b1c1fde740221a39ca4305292
LebronJay <1074642859@qq.com>
2020-01-29 19:32:07 +0800

    添加词条点赞收藏实体类

---
+++ src/main/java/com/colin/foodsource/model/EntryLikeCollect.java
@@ -0,0 +1,83 @@
+package com.colin.foodsource.model;
+
+import java.util.Date;
+
+/**
+ * @Description: 词条点赞收藏
+ * Created by Colin on 2020/1/29 0029 下午 7:28.
+ */
+public class EntryLikeCollect {
+
+    private String entryLikeCollectId;//点赞收藏编号
+    private String foodEntryId;//词条编号
+    private String likeState;//点赞状态
+    private Date likeDate;//点赞时间
+    private String collectState;//收藏状态
+    private Date collectDate;//收藏时间
+    private String oIdUser;//用户编号
+    private String state;//状态
+
+    public String getEntryLikeCollectId() {
+        return entryLikeCollectId;
+    }
+
+    public void setEntryLikeCollectId(String entryLikeCollectId) {
+        this.entryLikeCollectId = entryLikeCollectId;
+    }
+
+    public String getFoodEntryId() {
+        return foodEntryId;
+    }
+
+    public void setFoodEntryId(String foodEntryId) {
+        this.foodEntryId = foodEntryId;
+    }
+
+    public String getLikeState() {
+        return likeState;
+    }
+
+    public void setLikeState(String likeState) {
+        this.likeState = likeState;
+    }
+
+    public Date getLikeDate() {
+        return likeDate;
+    }
+
+    public void setLikeDate(Date likeDate) {
+        this.likeDate = likeDate;
+    }
+
+    public String getCollectState() {
+        return collectState;
+    }
+
+    public void setCollectState(String collectState) {
+        this.collectState = collectState;
+    }
+
+    public Date getCollectDate() {
+        return collectDate;
+    }
+
+    public void setCollectDate(Date collectDate) {
+        this.collectDate = collectDate;
+    }
+
+    public String getoIdUser() {
+        return oIdUser;
+    }
+
+    public void setoIdUser(String oIdUser) {
+        this.oIdUser = oIdUser;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
