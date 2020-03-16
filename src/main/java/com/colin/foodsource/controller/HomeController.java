693f0d7fa1f2ec46e8ee3a5a5d1582e275e39e5e
LebronJay <1074642859@qq.com>
2020-03-16 13:26:36 +0800

    添加首页信息控制器

---
+++ src/main/java/com/colin/foodsource/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.colin.foodsource.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: 首页信息
+ * Created by Colin on 2020/3/16 0016 下午 1:25.
+ */
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+}
