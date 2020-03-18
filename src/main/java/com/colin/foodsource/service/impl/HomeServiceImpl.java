c7ff9ea0972dcd9e5480ddbbdb9a25bcb1b40628
LebronJay <1074642859@qq.com>
2020-03-18 09:45:38 +0800

    新建首页接口实现类

---
+++ src/main/java/com/colin/foodsource/service/impl/HomeServiceImpl.java
@@ -0,0 +1,10 @@
+package com.colin.foodsource.service.impl;
+
+import com.colin.foodsource.service.HomeService;
+
+/**
+ * @Description: 首页信息服务接口实现类
+ * Created by Colin on 2020/3/17 0017 上午 11:12.
+ */
+public class HomeServiceImpl implements HomeService{
+}
