ec5e103b0a335334e3ee639f9e9ee0fa8612e049
LebronJay <1074642859@qq.com>
2020-03-21 20:31:26 +0800

    添加文章点赞收藏服务接口实现类

---
+++ src/main/java/com/colin/foodsource/service/impl/ArticleLikeCollectServiceImpl.java
@@ -0,0 +1,12 @@
+package com.colin.foodsource.service.impl;
+
+import com.colin.foodsource.service.ArticleLikeCollectService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description: 文章点赞收藏服务接口实现类
+ * Created by Colin on 2020/3/20 0020 上午 9:38.
+ */
+@Service
+public class ArticleLikeCollectServiceImpl implements ArticleLikeCollectService {
+}
