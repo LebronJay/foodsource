4a2a3791cb6b5eca6f282d1d417418342ca7449e
LebronJay <1074642859@qq.com>
2020-03-22 17:50:26 +0800

    添加文章点赞收藏服务控制器

---
+++ src/main/java/com/colin/foodsource/controller/ArticleLikeCollectController.java
@@ -0,0 +1,13 @@
+package com.colin.foodsource.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: 文章点赞收藏控制器
+ * Created by Colin on 2020/3/22 0022 下午 5:48.
+ */
+@RestController
+@RequestMapping("/articleLikeCollect")
+public class ArticleLikeCollectController {
+}
