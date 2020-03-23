a8a92ef273524ed92157be17d961c7d18f15607b
LebronJay <1074642859@qq.com>
2020-03-23 16:44:55 +0800

    热搜信息Mapper

---
+++ src/main/java/com/colin/foodsource/dao/HotSearchMapper.java
@@ -0,0 +1,13 @@
+package com.colin.foodsource.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Description: 热搜信息Mapper
+ * Created by Colin on 2020/3/23 0023 下午 4:44.
+ */
+@Repository
+@Mapper
+public interface HotSearchMapper {
+}
